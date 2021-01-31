7e6bfe0b500300743aef880bf03bb0fd3c29fa44
enars <ejnar.sorensen@gmail.com>
2021-02-01 00:28:58 +0100

    Fix firebase and sendMessage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import firebase from 'firebase/app'
@@ -18,7 +18,7 @@ firebase.initializeApp({
 })
 
 const auth = firebase.auth()
-const firestore = firestore.firestore()
+const firestore = firebase.firestore()
 
 function App() {
 
@@ -26,7 +26,7 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
       </header>
 
       <section>
@@ -60,19 +60,50 @@ function ChatRoom() {
   const query = messagesRef.orderBy('createdAt').limit(25)
 
   const [messages] = useCollectionData(query, {idField: 'id'})
+
+  const [formValue, setFormValue] = useState('')
+
+  const sendMessage = async(e) => {
+    e.preventDefault()
+
+    const {uid, photoURL} = auth.currentUser
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('')
+  }
   
   return(
-    <div>
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-    </div>
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
 
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value) } />
+        
+        <button type="submit">send</button>
+      </form>
+    </>
   )
 }
 
 function ChatMessage(props) {
-  const {text, uid} = props.message
+  const {text, uid, photoURL} = props.message
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received'
 
-  return <p>{text}</p>
+  return (
+  <div className={`message ${messageClass}`}>
+    <img src={photoURL}></img>
+    <p>{text}</p>
+    
+  </div>
+  ) 
 }
 
 
